1d03fc8f7ed9a90fa82c407002c5f6fb0638fdbd
Samuel Reed <samuel.trace.reed@gmail.com>
2017-04-21 11:55:37 -0500

    feat(startup): Display tag & git hash on startup

--- market_maker/utils/constants.py
+++ market_maker/utils/constants.py
@@ -1,3 +1,9 @@
+import subprocess
 # Constants
 XBt_TO_XBT = 100000000
-VERSION = '1.0'
+VERSION = 'v1.1'
+try:
+    VERSION = subprocess.check_output(["git", "describe", "--tags"])
+except Exception as e:
+    # git not available, ignore
+    pass
